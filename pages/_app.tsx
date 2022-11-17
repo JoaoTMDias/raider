ac0e4d5fc0776626fc91ae149670b30ce63ef1be
Joao Dias <joao.dias@feedzai.com>
2022-11-17 00:50:27 +0000

    feat(Data): Configured react query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,22 @@
 import "src/styles/globals.scss";
+import { useState } from "react";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Layout } from "@/components";
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <SessionProvider session={session}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <QueryClientProvider client={queryClient}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </SessionProvider>
   );
 }
