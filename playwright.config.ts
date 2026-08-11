edb7feac52b8ec7a7779811b987b394cca91927c
João Dias <contacto@joaodias.me>
2026-08-11 14:39:32 +0100

    chore: add pnpm workspace configuration with only built dependencies

--- playwright.config.ts
+++ playwright.config.ts
@@ -82,7 +82,7 @@ const CONFIG: PlaywrightTestConfig = {
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'yarn dev',
+    command: 'pnpm dev',
     url: 'http://localhost:3000',
     reuseExistingServer: !PLAYWRIGHT_CONFIG.CI,
   },
